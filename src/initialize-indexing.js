0f0698a368feb09501558ebc498caff38862467a
DawoodKhatri <dawoodkhatri18@gmail.com>
2024-11-20 18:45:22 +0530

    Updated Data

--- src/initialize-indexing.js
+++ src/initialize-indexing.js
@@ -1,19 +1,18 @@
 import { getDocuments, getAPIDocuments } from "./utils/getDocuments.js";
 import { createIndex, createAPIDocIndex } from "./utils/createIndex.js";
 
-// const documents = getDocuments();
+const documents = getDocuments();
 
-// (async () => {
-//   console.log("Initializing chat engine...");
-//   await createIndex(documents);
-//   console.log("Chat engine initialized");
-// })();
+(async () => {
+  console.log("Initializing chat engine...");
+  await createIndex(documents);
+  console.log("Chat engine initialized");
+})();
 
 (async () => {
   console.log("Initializing api docs indexing...");
-  const all_carrier_documents = getAPIDocuments();
+  const carrier_documents = getAPIDocuments();
 
-  const carrier_documents = all_carrier_documents.slice(0, 37);
   for (const carrier of carrier_documents) {
     console.log(
       `Progress ${carrier_documents.indexOf(carrier)}/${
