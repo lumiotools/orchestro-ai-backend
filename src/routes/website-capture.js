39ce9f82c8b7ec22c48ea7d315451440a0f208b1
DawoodKhatri <dawoodkhatri18@gmail.com>
2024-11-20 16:56:58 +0530

    Removed Web Capture API

--- src/routes/website-capture.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { handleWebsiteCapture } from "../controllers/website-capture.js";
-
-const route = Router();
-
-route.get("/website-capture", handleWebsiteCapture);
-
-export default route;
